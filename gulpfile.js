61780206379136f3485884a7f56c7968967aec17
CodingRony <ronysheer@gmail.com>
2015-12-22 11:48:34 +0200

    Add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,26 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util'),
-    compass = require('gulp-compass'),
     connect = require('gulp-connect');
 
 gulp.task('connect', function() {
   connect.server({
     root: 'builds/development',
     livereload: true
+    });
   });
-});
+
+  gulp.task('html', function() {
+    gulp.src('builds/development/*.html')
+    .pipe(connect.reload());
+  });
+
+  gulp.task('css', function() {
+    gulp.src('builds/development/css/*.css')
+    .pipe(connect.reload());
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch('builds/development/css/*.css', ['css']);
+    gulp.watch('builds/development/*.html', ['html']);
+  });
+
+  gulp.task('default', ['connect', 'css', 'watch', 'html']);
